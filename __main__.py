4d9ca88186ebd6cc000b84fa1765cd760e6024ac
Ryan Moua <ryanmoua@MacBook-Pro.local>
2018-09-12 21:23:07 -0700

    Update comments to be more complete

--- __main__.py
+++ __main__.py
@@ -61,7 +61,7 @@ def main():
         for i in range(3):
             deck_list2.add_card(monster)
 
-    #Set Up Duel
+    # These variables initialize the classes need to set up the Duel.
     deck1 = Deck(deck_list1)
     deck2 = Deck(deck_list2)
     hand1 = []
@@ -74,29 +74,37 @@ def main():
     lp2 = 8000
     turn_count = 0
     phase = "DP"
-
-    #Duel Start
-    deck1.shuffle()
-    deck2.shuffle()
+    turn_player_idx = 0
     player1 = Player(1, hand1, gy1, deck1, field1, lp1)
     player2 = Player(2, hand2, gy2, deck2, field2, lp2)
     players = [player1, player2]
+    winner = None
+
+    # These functions start the Duel using the variables defined above.
+    # Both decks are shuffled and both players draw 5 cards.
+    deck1.shuffle()
+    deck2.shuffle()
     player1.draw(5)
     player2.draw(5)
-    turn_player_idx = 0
-    winner = None
     print("DUEL START")
 
-    #Duel Loop
+    # This while loop contols the actions that the players can take in the
+    # Duel. It will not close until the Duel is over (i.e. a Player's 
+    # LifePoints hit 0, a Player can't draw any more cards, etc...)
     while player1.get_lp() > 0 and player2.get_lp() > 0 and winner == None:
-        #Print Field
+        # This section prints out the visualtion of the current state of
+        # of the Duel (i.e. Turn Count, Each Player's Hand, Field, LP, etc...)
         print("\n\n=====================================================")
         print("TURN: {}".format(turn_count + 1))
         turn_player = players[turn_player_idx]
         opposite_player_idx = abs(turn_player_idx - 1)
         opposite_player = players[abs(turn_player_idx - 1)]
 
-        #Draw Phase
+        # This section handles the logic of the Draw Phase.
+        # If the player can draw a card, they draw it. If not the game is over
+        # and the opponent wins. Also, in this phase all action locks are 
+        # reset. (i.e. Monsters not being able to Attack or change their
+        # Positions)
         phase = "DP"
         if turn_player.can_draw():
             print("Player {player_id_1} draws a card".format(
@@ -115,7 +123,10 @@ def main():
                 monster.unlock_position()
         turn_player.set_can_summon(True)
 
-        #Main Phase
+        # This section adds the intial actions the Player can take during
+        # the Main Phase. After choosing from the selections the user enters
+        # the actions while loop and choose from those choices until their
+        # turn is over.
         phase = "MP"
         possible_moves = []
         if turn_player.get_can_summon():
@@ -127,7 +138,8 @@ def main():
             possible_moves.append("Go to Battle Phase")
         possible_moves.append("End Turn")
 
-        #Possible Moves - User Choice Loop
+        # This loop tells the Player what actions they can currently take.
+        # It uses an input prompt so the Player can select an action.
         while len(possible_moves) > 0:
             print("OPPONENT'S FIELD:")
             print("{} LP".format(opposite_player.get_lp()))
@@ -149,7 +161,16 @@ def main():
             while choice >= len(possible_moves) or choice < 0:
                 choice = try_int_input("Please enter a valid choice: ")
 
-            #MAIN PHASE CHOICES
+            # This section handles the logic for actions the Player can take
+            # during their Main Phase.
+            # Summon: If able, summon a monster from your hand in Face-up
+            # Attack Position.
+            # Set: If able, set a monster from your hand in Face-down Defense
+            # Position.
+            # Change Battle Position: If able, change the Position of a
+            # monster you control. (i.e. Attack to Defense, Face-down Defense
+            # to Face-up Attack, etc...)
+            # Go to Battle Phase: Go to Battle Phase.
             if (possible_moves[choice] == "Summon" or 
                 possible_moves[choice] == "Set"):
                 possible_monsters = []
@@ -216,7 +237,11 @@ def main():
             elif possible_moves[choice] == "Go to Battle Phase":
                 phase = "BP"
             
-            #BATTLE PHASE CHOICES
+            # This section handles the logic for actions the Player can take
+            # during their Battle Phase.
+            # Attack: If able, the Player's monster can either attack the
+            # opponent's monster or the opponent directly if they have no
+            # monsters.
             elif possible_moves[choice] == "Attack":
                 possible_monsters = []
                 possible_monsters = turn_player.get_field().get_can_attack()
@@ -284,7 +309,13 @@ def main():
                             target_pos=target.get_position()
                         )
                     )
-                    ##ATTACK WIN
+                    # The following sections handle the logic of battle and 
+                    # its result. Depending on the result of battle a monster
+                    # may be destroyed, LifePoints may be lost, etc...
+
+                    # This section handles the logic for the battle scenario
+                    # in which the attacker wins against an Attack Position
+                    # target.
                     if battle_result[0] and battle_result[1] > 0:
                         print(
                             "{attacker} WINS. {target} DESTROYED"\
@@ -300,7 +331,9 @@ def main():
                         opposite_player.change_lp(-1 * battle_result[1])
                         attacker.lock_attack()
             
-                    ##ATTACK LOSE
+                    # This section handles the logic for the battle scenario
+                    # in which the attacker loses against an Attack Position
+                    # target.
                     elif battle_result[0] and battle_result[1] < 0:
                         print(
                             "{target} WINS. {attacker} DESTROYED"\
@@ -314,7 +347,9 @@ def main():
                         )
                         turn_player.change_lp(battle_result[1])
             
-                    ##ATTACK TIE
+                    # This section handles the logic for the battle scenario
+                    # in which the attacker ties with an Attack Position 
+                    # target.
                     elif battle_result[0] and battle_result[1] == 0:
                         print(
                             "BATTLE TIE. {attacker} and {target} DESTROYED"\
@@ -331,7 +366,9 @@ def main():
                                                         .remove(target_idx)
                         )
             
-                    ##DEF LOSE
+                    # This section handles the logic for the battle scenario
+                    # in which the attacker loses to a Defense Position 
+                    # target.
                     elif not battle_result[0] and battle_result[1] < 0:
                         print(
                             "{attacker} LOSES. {target} NOT DESTROYED"\
@@ -344,7 +381,10 @@ def main():
                         attacker.lock_attack()
                         if target.get_is_set():
                             target.flip()
-                    ##DEF WIN
+
+                    # This section handles the logic for the battle scenario
+                    # in which the attacker wins against a Defense Position 
+                    # target.
                     elif not battle_result[0] and battle_result[1] > 0:
                         print(
                             "{attacker} WINS. {target} DESTROYED"\
@@ -358,13 +398,19 @@ def main():
                                                         .remove(target_idx)
                         )
                         attacker.lock_attack()
-                    ##DEF TIE
+
+                    # This section handles the logic for the battle scenario
+                    # in which the attacker ties with a Defense Position 
+                    # target.
                     else:
                         print("NO MONSTERS DESTROYED")
                         if target.get_is_set():
                             target.flip()
                         attacker.lock_attack()
                 else:
+                    # This section handles the logic for the battle scenario
+                    # in which the opponent has no monsters and the attacker
+                    # attacks directly.
                     print(
                         "{attacker} attacks Player {player} directly"\
                         .format(
@@ -375,6 +421,9 @@ def main():
                     damage = -1 * attacker.get_atk()
                     opposite_player.change_lp(damage)
                     attacker.lock_attack()
+                # This section checks if either Players' LifePoints are 0
+                # after battle. If one of the Players' LifePoints is 0 the
+                # Duel ends and the other Player wins.
                 if turn_player.get_lp() <= 0:
                     turn_player.set_lp(0)
                     winner = opposite_player.get_id()
@@ -384,11 +433,15 @@ def main():
                     winner = turn_player.get_id()
                     break
 
-            #UNIVERSAL CHOICES
+            # This section handles the logic for actions the Player can take
+            # at any Phase in the Duel.
+            # End Turn: Go to End Phase and start opponent's turn.
             elif possible_moves[choice] == "End Turn":
                 phase = "EP"
 
-            #REST CHOICES
+            # This section clears the Player's possible actions and then adds
+            # new possible actions based on the game state.
+            # i.e. Game Phase, Turn Count, actions already taken, etc...
             possible_moves = []
             if phase == "MP":
                 if turn_player.get_can_summon():
@@ -404,8 +457,10 @@ def main():
                     possible_moves.append("Attack")
                 possible_moves.append("End Turn")
             elif phase =="EP":
-                #End Phase
-                #Check hand count > 6
+                # This section handles the logic of the End Phase.
+                # If players have more than 6 cards they must discard down to
+                # 6. The turn count is incremented and then the turn player
+                # is changed the opposite player.
                 while len(turn_player.get_hand()) > 6:
                     current_hand = turn_player.get_hand()
                     for i in range(len(current_hand)):
@@ -426,14 +481,12 @@ def main():
                     while choice > len(current_hand) or choice < 0:
                         choice = input("Please enter a valid choice: ")
                     current_hand.pop(choice)
-                ##Increment turn count
                 turn_count += 1
-                ## Change Player
                 turn_player_idx = opposite_player_idx
             else:
                 print("You shouldn't be here.")
     
-    #Game Over
+    # After the Duel loop ends, the name of the winner is printed.
     print("PLAYER {} WINS!".format(winner))
 
 if __name__ == '__main__':
